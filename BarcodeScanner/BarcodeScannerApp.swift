4d2c858852e35e857523c79c4c4a608da96bb5f6
BlayckPanthers <fabienlebon974@gmail.com>
2021-01-25 18:24:48 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Fabien Lebon on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
